ef5801a6fa4bca741e05cc7f1ee4358ce1fd7cd3
Craig A. Butler <wunderhund@gmail.com>
2019-10-06 18:02:45 -0600

    Misc additions and formatting

--- timedil.py
+++ timedil.py
@@ -13,12 +13,14 @@ def time_breakdown(time_in_years):
     hours, hours_rem = divmod(days_rem, 3600)
     minutes, seconds = divmod(hours_rem, 60)
 
-    time_string = f'{int(years):,}' + " years, " \
-                + str(int(months)) + " months, " \
-                + str(int(days)) + " days, " \
-                + str(int(hours)) + " hours, " \
-                + str(int(minutes)) + " minutes, " \
-                + str(int(seconds)) + " seconds"
+    time_string = (
+        f"{int(years):,} years, "
+        f"{int(months)} months, " 
+        f"{int(days)} days, " 
+        f"{int(hours)} hours, " 
+        f"{int(minutes)} minutes, " 
+        f"{int(seconds)} seconds"
+    )
     return time_string
 
 
@@ -35,10 +37,12 @@ def main():
                         help='Light-years to travel')
     parser.add_argument('-sl', '--shiplength', type=Decimal, default=10,
                         help='Length of your ship in meters')
-    parser.add_argument('-p', '--precision', type=int, default=10,
+    parser.add_argument('-p', '--precision', type=int, default=6,
                         help="Number of decimal places. Defaults to 6.")
-    parser.add_argument('-m', '--mass', type=Decimal, default=10,
-                        help="Mass of your ship in tons. Defaults to 10.")
+    parser.add_argument('-m', '--mass', type=Decimal, default=10000,
+                        help="Mass of your ship in kg. Defaults to 10000.")
+    parser.add_argument('-t', '--time', type=Decimal, default=0.5,
+                        help="Proportion of trip spent accelerating (0-1). Defaults to 0.5")
     args = parser.parse_args()
 
     if not args.speed and not args.meterspersec:
@@ -51,6 +55,7 @@ def main():
     c2 = c ** 2  # The speed of light, squared
     getcontext().prec = args.precision
 
+    # Calculate time of trip (in years) for non-traveling observer
     if args.speed:
         v = args.speed * c
         obs_time = args.lightyears/args.speed
@@ -65,25 +70,38 @@ def main():
         gamma = Decimal('Infinity')
         ship_time = 0
         ship_length = 0
+        ship_mass = args.mass * gamma
+        kinetic_energy_classic = Decimal('Infinity')
+        kinetic_energy_relative = Decimal('Infinity')
     else:
-        gamma = Decimal(1 / (abs(1 - B) ** 0.5))
-        ship_time = obs_time / gamma
+        gamma = Decimal(1 / ((abs(1 - B)) ** 0.5))
+        ship_time = Decimal(obs_time / gamma)
         ship_length = args.shiplength / gamma
-        
-    ship_mass = args.mass * gamma
+        ship_mass = args.mass * gamma
+        kinetic_energy_classic = Decimal(0.5) * args.mass * v2
+        kinetic_energy_relative = (args.mass * gamma * c2) - (args.mass * c2)
+
+    ship_energy = ship_mass * c2  # E=mc^2 
 
     time_diff = abs(obs_time - ship_time)
 
-    print("Lightyears to travel: ", args.lightyears)
-    print("Speed in m/s: ", f'{v:,}', "m/s")
-    print("Percent of c: ", (v / c)*100, "%")
-    print("Lorentz Factor: ", Decimal(gamma))
-    print("Observer Time: ", time_breakdown(obs_time))
-    print("Ship Time: ", time_breakdown(ship_time))
-    print("Difference in times: ", time_breakdown(time_diff))
-    print("Ship Length: ", ship_length, " meters")
-    print("Ship Mass: ", ship_mass, " tons")
+    output_items = [
+        f"-----------------------------------------",
+        f"Lightyears to travel: {args.lightyears}",
+        f"Speed in m/s: {v:,} m/s",
+        f"Percent of c: {v / c*100}%",
+        f"Lorentz Factor: {Decimal(gamma)}",
+        f"Observer Time: {time_breakdown(obs_time)}",
+        f"Ship Time: {time_breakdown(ship_time)}",
+        f"Difference in times: {time_breakdown(time_diff)}",
+        f"Ship Length: {ship_length} meters",
+        f"Ship Mass: {ship_mass} kg",
+        f"Ship Mass Energy Equivalent: {ship_energy} joules",
+        f"Kinetic Energy Required (Classic): {kinetic_energy_classic} joules",
+        f"Kinetic Energy Required (Relativity): {kinetic_energy_relative} joules",
+    ]
 
+    print("\n".join(output_items))
 
 if __name__ == "__main__":
     main()
